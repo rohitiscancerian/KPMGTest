84bb4fa263a87b33d0109c7e77773d2670c96a05
rohitiscancerian <er.rohitkumar@gmail.com>
2015-08-04 10:09:29 +0100

    Revert "Revert "Version1.0""
    This reverts commit 614e2120b6ca4b371d81fdada4ca120cd28ac0df.

---
+++ KPMG_Assignment/KPMG_Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KPMG_Assignment.MvcApplication" Language="C#" %>
---
+++ KPMG_Assignment/KPMG_Assignment/Validation/ValidateFileXlsAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KPMG_Assignment.Validation
+{
+    public class ValidateFileXlsAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            const int maxContent = 1024 * 1024 * 10;
+            var allowedExt = new string[] { ".xls", ".xlsx" };
+
+            var file = value as HttpPostedFileBase;
+
+            if (file == null)
+                return false;
+
+            // check type of file 
+            if (!allowedExt.ToList().Contains(file.FileName.Substring(file.FileName.LastIndexOf("."))))
+            {
+                ErrorMessage = "Please upload a file of type: " + string.Join(", ", allowedExt);
+                return false;
+            }
+
+            // check the length of the file
+            if (file.ContentLength > maxContent)
+            {
+                ErrorMessage = "Your file is too large, maximum allowed size is : " + (maxContent / 1024) + "MB";
+                return false;
+            }
+            return true;
+        }
+    }
+}
